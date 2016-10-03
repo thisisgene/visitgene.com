fd12e59a9515f9a0e36e4afa95f0a09643ace2df
thisisgene <emdo2000@gmail.com>
2016-10-03 03:37:36 +0200

    Created weird arrow.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,34 @@
     }, 1000);
   });
 
+$.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options === 'string') {
+        defaults.selector = options;
+      }
+
+      options = $.extend(defaults, options);
+      return $(options.selector).click(function (e) {
+        var jumpobj = $(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 1000;
+        var offset = $(target).offset().top;
+
+        $('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing', function(){
+          // setTimeout(function(){clickToActivate = false;}, 100);
+        });
+        e.preventDefault();
+      });
+    }
+  };
+
+  $(document).ready(function() {
+    $.mark.jump();
+  });
+
 })(jQuery);
